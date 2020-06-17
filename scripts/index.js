d504ce0b360c1300de82a958ffd2907358932089
Heymac <heymacsmith@yahoo.com>
2020-06-17 18:08:23 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
 		$(".mainTitle ul").hide();
 	});
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navBar")[0].style.top = "0";
+  } else {
+    document.getElementsByClassName("navBar")[0].style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
